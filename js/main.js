5f7dc638272d4cfb2a26ce005b674991b496039f
Andre Castro <andrefs.c@yahoo.com>
2022-07-18 14:38:33 +0100

    inserting more styles and more functions in js

--- js/main.js
+++ js/main.js
@@ -7,15 +7,35 @@ const Main = {
     },
 
     cacheSelectors: function() {
-        this.checkButtons = document.querySelectorAll('.check')
+        this.$checkButtons = document.querySelectorAll('.check')
     },
 
     bindEvents: function() {
-        this.checkButtons.forEach(function(button){
-            button.onclick = function(){
-                alert('ok')
-            }
+        const self = this
+        
+        this.$checkButtons.forEach(function(button){
+            button.onclick = self.Events.checkButton_click
         })
+    },
+
+
+    Events: {
+        checkButton_click: function(e) {
+            const li = e.target.parentElement
+
+            const $check = document.querySelectorAll('.check')
+
+            const isDone = li.classList.contains('done')
+
+            if (!isDone) {
+                li.classList.add('done')
+
+                return
+            }
+
+            li.classList.remove('done')
+            
+        }
     }
 
 }
